9b9e84c67ddebd24cf5fe18d8fdf23e4650f4b5d
Star Huang <star@beansmile.com>
2014-05-01 10:04:30 +0800

    add update action for article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -25,6 +25,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
