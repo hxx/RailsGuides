b85ff246646c39aea567677164f71343fa4335c1
Star Huang <star@beansmile.com>
2014-05-01 09:33:23 +0800

    add index action for article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
