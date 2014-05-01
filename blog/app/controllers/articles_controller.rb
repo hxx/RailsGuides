d0bd4434761b661bcbf16dd3e379ef6bc297bf5b
Star Huang <star@beansmile.com>
2014-05-01 09:30:12 +0800

    add show action for article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -8,4 +8,13 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
