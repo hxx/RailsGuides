ff31c36806c6775c599c603bdc49750621030f8d
Star Huang <star@beansmile.com>
2014-05-01 09:29:54 +0800

    add the create action for article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
 end
