5478abdf610b2266a9e5987dfb75621625ab7f91
hxx <845059331@qq.com>
2014-05-02 12:17:39 +0800

    rake db:migrate

---
+++ ActiveRecord/db/schema.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140502032901) do
+
+  create_table "customers_products", id: false, force: true do |t|
+    t.integer "customer_id", null: false
+    t.integer "product_id",  null: false
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.decimal  "price",         precision: 5, scale: 2
+    t.integer  "supplier_id"
+    t.string   "supplier_type"
+  end
+
+  add_index "products", ["supplier_id", "supplier_type"], name: "index_products_on_supplier_id_and_supplier_type"
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
+
+end
