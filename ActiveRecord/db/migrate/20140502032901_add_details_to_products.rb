14d1dba5880fdc4bed36c429a682109105ed191b
Star Huang <star@beansmile.com>
2014-05-02 11:31:56 +0800

    Supported Type Modifiers

---
+++ ActiveRecord/db/migrate/20140502032901_add_details_to_products.rb
@@ -0,0 +1,6 @@
+class AddDetailsToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :price, :decimal, precision: 5, scale: 2
+    add_reference :products, :supplier, polymorphic: true, index: true
+  end
+end
