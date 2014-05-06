0c9154aa0884a5af218a7aad0737e2574111b95b
yellowstar <845059331@qq.com>
2014-05-06 20:46:19 +0800

    scope module: and routing
    route /posts (without the prefix /admin) to Admin::PostsController

--- ActionController/config/routes.rb
+++ ActionController/config/routes.rb
@@ -22,6 +22,13 @@ Rails.application.routes.draw do
     resources :posts, :comments
   end
 
+  # scope module:
+  scope module: 'admin' do
+    resources :posts, :comments
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
