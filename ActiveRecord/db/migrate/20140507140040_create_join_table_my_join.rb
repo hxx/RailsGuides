3b4267b5999c63038679d1af48cbc411f8f3c3ed
yellowstar <845059331@qq.com>
2014-05-07 22:34:01 +0800

    remove the conflict of join_table CustomersProducts

--- ActiveRecord/db/migrate/20140507140040_create_join_table_my_join.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableMyJoin < ActiveRecord::Migration
-  def change
-    create_join_table :customers, :products do |t|
-      # t.index [:customer_id, :product_id]
-      # t.index [:product_id, :customer_id]
-    end
-  end
-end
