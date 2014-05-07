f0962c387954da7144e73af5af4ebdc1f149fb8e
yellowstar <845059331@qq.com>
2014-05-07 22:32:55 +0800

    Changing the Output of Running Migrations

--- ActiveRecord/db/schema.rb
+++ ActiveRecord/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507135846) do
+ActiveRecord::Schema.define(version: 20140507143102) do
 
   create_table "another_products", force: true do |t|
     t.string "name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20140507135846) do
     t.integer "customer_id", null: false
     t.integer "product_id",  null: false
   end
+
+  create_table "examples", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "examples", ["name"], name: "index_examples_on_name"
 
   create_table "overridings", force: true do |t|
     t.string  "part_number"
