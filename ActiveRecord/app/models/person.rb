06a86b081a0829bd7e47ea334833f4d5c5dd191a
yellowstar <845059331@qq.com>
2014-05-08 22:01:59 +0800

    change :accept option of acceptance

--- ActiveRecord/app/models/person.rb
+++ ActiveRecord/app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
   validates :name, presence: true
   # acceptance
-  validates :terms_of_service, acceptance: true
+  # validates :terms_of_service, acceptance: true
+  validates :terms_of_service, acceptance: { accept: 'yes'}
 end
