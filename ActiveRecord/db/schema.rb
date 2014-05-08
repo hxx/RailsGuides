666ce3c6af3658743cfdb644e7f8c37a09ffed27
yellowstar <845059331@qq.com>
2014-05-08 21:43:34 +0800

    rails g model Person name:string

--- ActiveRecord/db/schema.rb
+++ ActiveRecord/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507143102) do
+ActiveRecord::Schema.define(version: 20140508134257) do
 
   create_table "another_products", force: true do |t|
     t.string "name"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20140507143102) do
   end
 
   add_index "overridings", ["user_id"], name: "index_overridings_on_user_id"
+
+  create_table "people", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "name"
