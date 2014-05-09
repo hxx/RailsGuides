0c130d9338151bf8bb7a51e4b721110e268de830
yellowstar <845059331@qq.com>
2014-05-09 21:59:15 +0800

    register callbacks on certain life cycle events

--- ActiveRecord/app/models/user.rb
+++ ActiveRecord/app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   validates :login, :email, presence: true
   before_validation :ensure_login_has_a_value
 
+  # register callbacks on certain life cycle events
+  before_validation :normalize_name, on: :before_create
+
+  # :on takes an array as well
+  after_validation :set_location, on: [ :create, :update ]
+
   # macro-style class methods
   before_create do
     self.name = login.capitalize if name.blank?
@@ -13,5 +19,13 @@ class User < ActiveRecord::Base
       if login.nil?
         self.login = email unless email.blank
       end
+    end
+
+    def normalize_name
+      self.name = self.name.downcase.titleize
+    end
+
+    def set_location
+      self.location = LocationService.query(self)
     end
 end
