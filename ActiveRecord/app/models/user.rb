5e53bdc34a196fede3c7e3a722524e8d115a1c98
yellowstar <845059331@qq.com>
2014-05-09 21:52:48 +0800

    macro-style class methods for callback registration

--- ActiveRecord/app/models/user.rb
+++ ActiveRecord/app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   validates :login, :email, presence: true
   before_validation :ensure_login_has_a_value
 
+  # macro-style class methods
+  before_create do
+    self.name = login.capitalize if name.blank?
+  end
+
   # ordinary method for implementing a callback
   protected
     def ensure_login_has_a_value
