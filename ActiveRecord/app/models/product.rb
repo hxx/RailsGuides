adce9c28f5e1b47191270400037503b7725e6b1f
yellowstar <845059331@qq.com>
2014-05-09 22:09:09 +0800

    after_initialize and after_find

--- ActiveRecord/app/models/product.rb
+++ ActiveRecord/app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  # after_initialize
+  after_initialize do |product|
+    puts "You have initialized an object!"
+  end
+
+  # after_find
+  after_find do |product|
+    puts "You have found an object!"
+  end
 end
