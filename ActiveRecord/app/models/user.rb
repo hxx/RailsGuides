da61e534e740a63e210c8e573c90c0b0ebf8b2eb
yellowstar <845059331@qq.com>
2014-05-09 21:48:54 +0800

    ordinary method for implementing a callback

--- ActiveRecord/app/models/user.rb
+++ ActiveRecord/app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
+  validates :login, :email, presence: true
+  before_validation :ensure_login_has_a_value
+
+  # ordinary method for implementing a callback
+  protected
+    def ensure_login_has_a_value
+      if login.nil?
+        self.login = email unless email.blank
+      end
+    end
 end
